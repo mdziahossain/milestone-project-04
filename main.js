c462c51c9e7ba6dfee03bfba0f42b7cfd526ecb9
Md Zia Hossain <mdziahossain@yandex.com>
2021-01-30 14:45:15 +0600

    Milestone Project-04 v2.0

---
+++ main.js
@@ -0,0 +1,12 @@
+// Ticket Cost Calculation
+        // let ticketTotal = 0;
+
+        // if (ticket == "firstClass" ) {
+        //     ticketTotal = ticketNewCount * 150;
+        // }
+        // if (ticket == "economyClass") {
+        //     ticketTotal = ticketNewCount * 100;   
+        // }
+        // document.getElementById(ticket + "-total").innerText = `$${ticketTotal}`;
+
+        
